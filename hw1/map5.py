629e68f1352909af85a995190f8fc6437d1bee56
s.latatye <s.latatye@tinkoff.ru>
2017-12-26 14:15:03 +0300

    hw1

--- hw1/map5.py
+++ hw1/map5.py
@@ -3,9 +3,7 @@
 import sys
 
 for line in sys.stdin:
-    r = line.split()
-    antiNucleus = int(r[0][:-1])
-    eventFile = int(r[1][:-1])
-    prodTime = double(r[10][:-1])
-    Pt = float(r[11][:-1])
-    print(‘{}\t{}\t{}\t{}’.format(antiNucleus, eventFile, prodTime, Pt))
+    row = line.split()
+    antiNucleus = int(row[0])
+    eventFile = int(row[1])
+    print('%s\t%s' % (antiNucleus, eventFile))
--- hw1/reduce2.py
+++ hw1/reduce2.py
@@ -2,10 +2,28 @@
 # coding=utf-8
 import sys
 
+current_elem = None
+current_sum = 0.0
+current_count = 0
+elem = None
+
 for line in sys.stdin:
-    r = line.split()
-    antiNucleus = int(r[0][:-1])
-    eventFile = int(r[1][:-1])
-    prodTime = double(r[10][:-1])
-    Pt = float(r[11][:-1])
-    print(‘{}\t{}\t{}\t{}’.format(antiNucleus, eventFile, prodTime, Pt))
+    r = line.split('\t')
+    antiNucleus = int(r[0])
+    prodTime = double(r[1])
+    elem = antiNucleus
+
+    if current_elem is None:
+    	current_sum = prodTime
+        current_count = 1
+        current_elem = elem
+    elif current_elem != elem:
+    	mean = current_sum / current_count
+        print('{}\t{}\t{}'.format(str(current_elem), str(mean), 'mean'))
+    elif current_elem == elem:
+        current_count += 1
+        current_sum += prodTime
+            
+if current_elem == elem:
+    mean = current_sum / current_count
+    print('{}\t{}\t{}'.format(str(current_elem), str(mean), 'mean'))
